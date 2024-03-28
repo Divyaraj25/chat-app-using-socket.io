f03a3882d27394c753ad6049c50369ec9bc405e0
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-03-28 12:04:52 +0530

    without unnecessary comments

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -7,6 +7,5 @@ socket.on("message", (msg) => {
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   const message = e.target.elements.message// taking input from user, message is name of input:text
-  // socket.emit("sendmsg", document.querySelector("#msg").value); // taking input from queryselector
   socket.emit("sendmsg", message.value);
 });
